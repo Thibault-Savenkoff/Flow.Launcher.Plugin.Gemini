b83eceab724292e0e80e18bb105bc38928b22b68
Thibault Savenkoff <thibault.savenkoff@outlook.fr>
2025-02-23 01:05:23 +0100

    Re-add Open Url fonctions

--- plugin/main.py
+++ plugin/main.py
@@ -1,17 +1,44 @@
-from pyflowlauncher import Plugin, Result, send_results
+from pyflowlauncher import Plugin, Result, Method
 from pyflowlauncher.result import ResultResponse
+import webbrowser
 
 plugin = Plugin()
 
+def open_url(self, url):
+    webbrowser.open(url)
 
-@plugin.on_method
-def query(query: str) -> ResultResponse:
-    r = Result(
-        Title="This is a title!",
-        SubTitle="This is the subtitle!",
-        IcoPath="icon.png"
-    )
-    return send_results([r])
+class Query(Method):
 
+    def __call__(self, query: str) -> ResultResponse:
+        r = Result(
+            Title="This is a title!",
+            SubTitle="This is the subtitle!",
+            IcoPath="Images/app.png",
+            JsonRPCAction={
+                "method": "open_url",
+                "parameters": ["https://github.com/Thibault-Savenkoff/Thibault-Savenkoff"]
+            },
+            Score= 0
+        )
+        self.add_result(r)
+        return self.return_results()
 
+class ContextMenu(Method):
+
+    def __call__(self, query: str) -> ResultResponse:
+        r = Result(
+            Title="This is a title!",
+            SubTitle="This is the subtitle!",
+            IcoPath="Images/app.png",
+            JsonRPCAction={
+                "method": "open_url",
+                "parameters": ["https://github.com/Thibault-Savenkoff/Flow.Launcher.Plugin.Gemini"]
+            },
+            Score= 0
+        )
+        self.add_result(r)
+        return self.return_results()
+
+plugin.add_method(Query())
+plugin.add_method(ContextMenu())
 plugin.run()
