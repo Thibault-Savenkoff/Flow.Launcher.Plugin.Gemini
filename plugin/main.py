1faa3a7ba0911b8b6d0864228ed0dd0016ad5ee9
Thibault Savenkoff <thibault.savenkoff@outlook.fr>
2025-02-23 01:18:24 +0100

    Cancel

--- plugin/main.py
+++ plugin/main.py
@@ -1,36 +1,26 @@
 from pyflowlauncher import Plugin, Result, Method
 from pyflowlauncher.result import ResultResponse
-import webbrowser
 
 plugin = Plugin()
 
-def open_url(self, url):
-    webbrowser.open(url)
-
 class Query(Method):
-    def open_url(self, url):
-        webbrowser.open(url)
 
     def __call__(self, query: str) -> ResultResponse:
         r = Result(
             Title="This is a title!",
             SubTitle="This is the subtitle!",
-            IcoPath="Images/app.png",
-            JsonRPCAction={"method": "open_url", "parameters": ["https://github.com/Thibault-Savenkoff/Thibault-Savenkoff"]}
+            IcoPath="Images/app.png"
         )
         self.add_result(r)
         return self.return_results()
 
 class ContextMenu(Method):
-    def open_url(self, url):
-        webbrowser.open(url)
 
     def __call__(self, query: str) -> ResultResponse:
         r = Result(
             Title="This is a title!",
             SubTitle="This is the subtitle!",
-            IcoPath="Images/app.png",
-            JsonRPCAction={"method": "open_url", "parameters": ["https://github.com/Thibault-Savenkoff/Flow.Launcher.Plugin.Gemini"]}
+            IcoPath="Images/app.png"
         )
         self.add_result(r)
         return self.return_results()
